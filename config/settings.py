48a8afc9ac78d030898435a842ea934839e93dbf
suleymankaya21 <suleyman.kaya@zealicsolutions.com>
2023-09-28 17:03:40 +0200

    Update database password env name

--- config/settings.py
+++ config/settings.py
@@ -90,7 +90,7 @@ DATABASES = {
         'PORT': os.environ.get('DB_PORT'),
         'NAME': os.environ.get('DB_NAME'),
         'USER': os.environ.get('DB_USER'),
-        'PASSWORD': os.environ.get('DB_PASSWORD'),
+        'PASSWORD': os.environ.get('DB_PASS'),
     }
 }
 
